20dac88d56ad98565b8376fb2ab75238d950a2e9
Eric <haowel8@uci.edu>
2022-11-20 01:45:27 -0800

    ex7

---
+++ ex7/client/src/footer.js
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+function footer() {
+    return (
+        <>
+            <div className="footer">
+                <p>Footer</p>
+            </div>
+        </>
+    );
+}
+
+export default footer;
